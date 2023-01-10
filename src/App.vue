4541f30094d93243b7fc8161355705229f4f9aa9
ManakiX <sogeyanaluntu@gmail.com>
2023-01-10 12:19:09 +0200

    NavBar added

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/projects">Projects</router-link> |
-    <router-link to="/resume">Resume</router-link> |
-    <router-link to="/testimonials">Testimonials</router-link> |
-    <router-link to="/contact">Contact</router-link> |
-  </nav>
+ <Navbar/>
   <router-view/>
 </template>
+
+<script>
+import Navbar from './components/NavBar.vue'
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
 
 <style>
 #app {
---
+++ src/components/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+    <nav id="nav" class="navbar bg-light sticky-top shadow-sm">
+        <div class="container-lg">
+          <img src="../assets/pngegg.png" alt="logo" width="30" height="30px" >
+          <a class="navbar-brand text-black fw-bold fs-4" href="#">RSA</a>
+          <div class="dropdown">
+            <button class="btn btn-secondary btn-dark px-3" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <span class="navbar-toggler-icon"></span>
+             <i class="fas fa-bars"></i>
+        
+            </button>
+            <ul class="dropdown-menu">
+              <li><router-link to ="/">Home</router-link></li>
+              <li><router-link to ="/about">About</router-link></li>
+              <li><router-link to ="/projects">Projects</router-link></li>
+              <li><router-link to ="/contacts">Contacts</router-link></li>
+              <li><router-link to ="/testimonials">Testimonials</router-link>></li>
+              <li><router-link to ="/socials">Socials</router-link></li>
+              <li><router-link to ="/education">Education</router-link></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+</template>
+
+<style>
+</style>
+
+<script>
+</script>
