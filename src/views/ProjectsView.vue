85b4d1f29948466402376c3d13146085e6a0a9e1
ManakiX <sogeyanaluntu@gmail.com>
2023-01-12 10:09:39 +0200

    arrays

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,47 +1,75 @@
 <template>
-    <section class="projects py-5" id="projects">
-        <div class="container-lg py-4">
-            <div class="row justify-content-center">
-                <div class="col-lg-8">
-                    <div class="section-title text-center">
-                        <h1 class="fw-bold mb-5">Latest Projects</h1>
+    <div>
+        <section class="projects py-5" id="projects">
+            <div class="container-lg py-4">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8">
+                        <div class="section-title text-center">
+                            <h1 class="fw-bold mb-5">Latest Projects</h1>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 col-lg-4">
+                        <div class="projects-item"></div>
+                        <img src="https://i.postimg.cc/Fzjrgc0c/Luffy-Flip-card-final.png" class="w-100 img-thumbnail" alt="projects-item">
+                        <h2 class="text-capitalized fs-5 my-2">Flip card</h2>
+                        <p class="mb4"><a href="https://silver-eclair-a67fc6.netlify.app/" target="_blank" class="text-info text-decoration-none">Live Demo</a></p>
+                    </div>
+                    <div class="col-md-6 col-lg-4">
+                        <div class="projects-item"></div>
+                        <img src="https://i.postimg.cc/nz1y2kCv/Screenshot-2022-11-01-090450.png" class="w-100 img-thumbnail" alt="projects-item">
+                        <h2 class="text-capitalized fs-5 my-2">E-commerce Site</h2>
+                        <p class="mb4"><a href="https://manakixiv.netlify.app/" target="_blank" class="text-info text-decoration-none">Live Demo</a></p>
+                    </div>
+                    <div class="col-md-6 col-lg-4">
+                        <div class="projects-item"></div>
+                        <img src="https://i.postimg.cc/MHmpqYxz/portfolio.png" class="w-100 img-thumbnail" alt="projects-item">
+                        <h2 class="text-capitalized fs-5 my-2">Bootstrap/CSS/HTML Portfolio</h2>
+                        <p class="mb4"><a href="https://luntu-portfolio.netlify.app/" target="_blank"  class="text-info text-decoration-none">Live Demo</a></p>
+                    </div>
+                    <div class="col-md-6 col-lg-4">
+                        <div class="projects-item"></div>
+                        <img src="https://i.postimg.cc/FsmnHcCS/Simple-Website.png" class="w-100 img-thumbnail" alt="projects-item">
+                        <h2 class="text-capitalized fs-5 my-2">Simple Website</h2>
+                        <p class="mb4"><a href="https://2ndweekrevision.netlify.app/" target="_blank"  class="text-info text-decoration-none">Live Demo</a></p>
                     </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-md-6 col-lg-4">
-                    <div class="projects-item"></div>
-                    <img src="https://i.postimg.cc/Fzjrgc0c/Luffy-Flip-card-final.png" class="w-100 img-thumbnail" alt="projects-item">
-                    <h2 class="text-capitalized fs-5 my-2">Flip card</h2>
-                    <p class="mb4"><a href="https://silver-eclair-a67fc6.netlify.app/" target="_blank" class="text-info text-decoration-none">Live Demo</a></p>
-                </div>
-                <div class="col-md-6 col-lg-4">
-                    <div class="projects-item"></div>
-                    <img src="https://i.postimg.cc/nz1y2kCv/Screenshot-2022-11-01-090450.png" class="w-100 img-thumbnail" alt="projects-item">
-                    <h2 class="text-capitalized fs-5 my-2">E-commerce Site</h2>
-                    <p class="mb4"><a href="https://manakixiv.netlify.app/" target="_blank" class="text-info text-decoration-none">Live Demo</a></p>
-                </div>
-                <div class="col-md-6 col-lg-4">
-                    <div class="projects-item"></div>
-                    <img src="https://i.postimg.cc/MHmpqYxz/portfolio.png" class="w-100 img-thumbnail" alt="projects-item">
-                    <h2 class="text-capitalized fs-5 my-2">Bootstrap/CSS/HTML Portfolio</h2>
-                    <p class="mb4"><a href="https://luntu-portfolio.netlify.app/" target="_blank"  class="text-info text-decoration-none">Live Demo</a></p>
-                </div>
-                <div class="col-md-6 col-lg-4">
-                    <div class="projects-item"></div>
-                    <img src="https://i.postimg.cc/FsmnHcCS/Simple-Website.png" class="w-100 img-thumbnail" alt="projects-item">
-                    <h2 class="text-capitalized fs-5 my-2">Simple Website</h2>
-                    <p class="mb4"><a href="https://2ndweekrevision.netlify.app/" target="_blank"  class="text-info text-decoration-none">Live Demo</a></p>
-                </div>
-            </div>
-        </div>
-    </section>
+        </section>
+    </div>
 </template>
-
 <script>
-
+export default {
+    name: 'projects',
+    data(){
+        return{
+            projectsArray: [
+                {
+                    projectName: "Flip-card",
+                    projectImage: "https://i.postimg.cc/Fzjrgc0c/Luffy-Flip-card-final.png",
+                    demoLink: "https://silver-eclair-a67fc6.netlify.app/",
+                },
+                {
+                    projectName: "E-commerce Site",
+                    projectImage: "https://i.postimg.cc/nz1y2kCv/Screenshot-2022-11-01-090450.png",
+                    demoLink:"https://manakixiv.netlify.app/",
+                },
+                {
+                    projectName: "Bootstrap/CSS/HTML Portfolio",
+                    projectImage: "https://i.postimg.cc/MHmpqYxz/portfolio.png",
+                    demoLink:"https://luntu-portfolio.netlify.app/",
+                },
+                {
+                    projectName: "Simple Website",
+                    projectImage: "https://i.postimg.cc/FsmnHcCS/Simple-Website.png",
+                    demoLink:"https://2ndweekrevision.netlify.app/",
+                }
+            ]
+        }
+    }
+}
 </script>
-
-<style>
-
+<style lang="">
+    
 </style>
--- src/views/TestimonialsView.vue
+++ src/views/TestimonialsView.vue
@@ -1,5 +1,5 @@
 <template>
-    <section id="testimonial">
+    <!-- <section id="testimonial">
         <div class="container-lg">
             <div class="row justify-content-center">
                 <div class="col-lg-8">
@@ -64,14 +64,54 @@
         <footer>
             Copyright &copy;2022
           </footer>
-    </section>
+    </section> -->
 
 </template>
 
 <script>
+export default {
+    name: 'testimonials',
+    data(){
+        return{
+            testimonialsArray: [
+    {
+        name: "Jeff Ricardo",
+        occupation:"Student",
+        image:"https://i.postimg.cc/hhWf3RYy/IMG-20221103-WA0005.jpg",
+        testimonial:"Luntu is one of a kind, he has ideas you would think are impossible, until he shows you in the idea in it's fullest and you realize, he's a genius.He has a bright future in front of him and he's always willing to learn new types of things to broaden up he's knowledge.",
+    },
+    {
+        name: "Sakhe Mzolisa",
+        occupation:"Student",
+        image:"https://i.postimg.cc/Kz8bHftL/Sakhe.jpg",
+        testimonial:"Luntu is one of the most honest people you can come across. Very wise beyond his years with bright ideas which could shake the world in a very positive way. A man who is not afraid to live and speak his truth.",
+    },
+    {
+        name: "Joel Mukanya",
+        occupation:"Lecturer",
+        image:"https://i.postimg.cc/13xJQg6X/image.jpg",
+        testimonial:"Luntu is a very creative, and well-behaved student. He does enjoy working with his classmates and works well on his project.He has gained knowledge of web development, including Bootstrap 5. I will be very delighted to see him get hired as a developer.",
+    },
+    {
+        name: "Timothy Jennike",
+        occupation:"Student",
+        image:"https://i.postimg.cc/1zH5cqPj/IMG-3573.jpg",
+        testimonial:"Luntu is a hard-working guy who can accomplish anything he puts his mind to. He has a very friendly personality which makes it very easy to approach him. He is very dedicated and committed to any project that he has set for himself.",
+    },
+    {
+        name: "Athenkosi Freddie",
+        occupation:"Student",
+        image:"https://i.postimg.cc/YSgSPCg1/IMG-20221004-074457-848.jpg",
+        testimonial:"Luntu is a serious & fun individual who enjoys learning and exploring different things. He enthusiasm into his work allows him to be creative with the littlest of things, and never seems to fail to make the environment quite lively. You won't regret having him as part of your team.",
+    }]
+        }
+    }
+}
+
 
 </script>
 
-<style>
+
+<style scoped>
 
 </style>
